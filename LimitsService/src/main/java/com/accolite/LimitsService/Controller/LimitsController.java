2cd9bd44be6737033f3a2d02e993e156d608f7f3
Anantha Lakshmi M V <anantha.mv@accolite.com>
2022-06-28 15:30:06 +0530

    simple microservice

---
+++ LimitsService/src/main/java/com/accolite/LimitsService/Controller/LimitsController.java
@@ -0,0 +1,22 @@
+package com.accolite.LimitsService.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.accolite.LimitsService.Configuration.Configuration;
+import com.accolite.LimitsService.bean.Limits;
+
+@RestController
+public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+		//return new Limits(1,1000);
+		return new Limits(configuration.getMinimum(),configuration.getMaximum());
+	}
+
+}
